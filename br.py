3c1d4ab6346670dc8db84b3678851fe7e228a84e
IT_8 <lymar.b@academy.com.ua>
2025-01-27 15:05:57 +0200

    xz

---
+++ br.py
@@ -0,0 +1,11 @@
+def check_password(pasword):
+    correct_password = "dB7nQ!oV"
+    if password == correct_password:
+        return True
+    return False
+
+def brute_force(password_list):
+    for password in password_list:
+        if check_password(password):
+            print("ACCESS GRANTED! Password is {password}")
+            return password
